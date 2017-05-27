37905233691f2c9da19da901ad9d91c14bca1b8f
ngrasset <ngrasset@student.42.fr>
2017-05-27 15:32:44 +0200

    [UPD] small changes to message.js

--- src/message.js
+++ src/message.js
@@ -19,38 +19,27 @@ const replyMessage = (message) => {
 
   // Call Recast.AI SDK, through /converse route
   request.converseText(text, { conversationToken: senderId })
-  .then(result => {
-    /*
-    * YOUR OWN CODE
-    * Here, you can add your own process.
-    * Ex: You can call any external API
-    * Or: Update your mongo DB
-    * etc...
-    */
-    if (result.action) {
-      console.log('The conversation action is: ', result.action.slug)
-    }
+    .then(result => {
+      if (result.action) {
+        console.log('The conversation action is: ', result.action.slug)
+        // If there is not any message return by Recast.AI for this current conversation
+        if (!result.replies.length) {
+          message.addReply({ type: 'text', content: 'I don\'t have the reply to this yet :)' })
+        } else {
+          // Add each reply received from API to replies stack
+          result.replies.forEach(replyContent => message.addReply({ type: 'text', content: replyContent }))
+        }
 
-    // If there is not any message return by Recast.AI for this current conversation
-    if (!result.replies.length) {
-      message.addReply({ type: 'text', content: 'I don\'t have the reply to this yet :)' })
-    } else {
-      // Add each reply received from API to replies stack
-      result.replies.forEach(replyContent => message.addReply({ type: 'text', content: replyContent }))
-    }
-
-    // Send all replies
-    message.reply()
-    .then(() => {
-      // Do some code after sending messages
+        // Send all replies
+        message.reply()
+          .catch(err => {
+            console.error('Error while sending message to channel', err)
+          })
+      }
     })
     .catch(err => {
-      console.error('Error while sending message to channel', err)
+      console.error('Error while sending message to Recast.AI', err)
     })
-  })
-  .catch(err => {
-    console.error('Error while sending message to Recast.AI', err)
-  })
 }
 
 module.exports = replyMessage
