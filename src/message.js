4a990096c6ea1f280be4232bd1b85f267991d8d1
PE Lieb <lieb.pe@gmail.com>
2017-07-01 14:18:54 +0200

    [UDP] arrays genre

--- src/message.js
+++ src/message.js
@@ -191,44 +191,44 @@ const startSearchFlow = (message, conversation) => {
 }
 
 const movieGenresQuick = [
-    { id: 28, name: 'Action'  },
-    { id: 12, name: 'Adventure'  },
-    { id: 16, name: 'Animation'  },
-    { id: 35, name: 'Comedy'  },
-    { id: 80, name: 'Crime'  },
-    { id: 99, name: 'Documentary'  },
-    { id: 18, name: 'Drama'  },
-    { id: 10751, name: 'Family'  },
-    { id: 14, name: 'Fantasy'  },
-    { id: 36, name: 'History'  },
-    { id: 27, name: 'Horror'  },
-    { id: 10402, name: 'Music'  },
-    { id: 9648, name: 'Mystery'  },
-    { id: 10749, name: 'Romance'  },
-    { id: 878, name: 'Science Fiction'  },
-    { id: 10770, name: 'TV Movie'  },
-    { id: 53, name: 'Thriller'  },
-    { id: 10752, name: 'War'  },
-    { id: 37, name: 'Western'  },
+    { name: 'Action'  },
+    { name: 'Adventure'  },
+    { name: 'Animation'  },
+    { name: 'Comedy'  },
+    { name: 'Crime'  },
+    { name: 'Documentary'  },
+    { name: 'Drama'  },
+    { name: 'Family'  },
+    { name: 'Fantasy'  },
+    { name: 'History'  },
+    { name: 'Horror'  },
+    { name: 'Music'  },
+    { name: 'Mystery'  },
+    { name: 'Romance'  },
+    { name: 'Science Fiction'  },
+    { name: 'TV Movie'  },
+    { name: 'Thriller'  },
+    { name: 'War'  },
+    { name: 'Western'  },
 ]
 
 const tvGenresQuick = [
-    { id: 10759, name: 'Action & Adventure'  },
-    { id: 16, name: 'Animation'  },
-    { id: 35, name: 'Comedy'  },
-    { id: 80, name: 'Crime'  },
-    { id: 99, name: 'Documentary'  },
-    { id: 18, name: 'Drama'  },
-    { id: 10751, name: 'Family'  },
-    { id: 10762, name: 'Kids'  },
-    { id: 9648, name: 'Mystery'  },
-    { id: 10763, name: 'News'  },
-    { id: 10764, name: 'Reality'  },
-    { id: 10765, name: 'Sci-Fi & Fantasy'  },
-    { id: 10766, name: 'Soap'  },
-    { id: 10767, name: 'Talk'  },
-    { id: 10768, name: 'War & Politics'  },
-    { id: 37, name: 'Western'  },
+    { name: 'Action & Adventure' },
+    { name: 'Animation' },
+    { name: 'Comedy' },
+    { name: 'Crime' },
+    { name: 'Documentary' },
+    { name: 'Drama' },
+    { name: 'Family' },
+    { name: 'Kids' },
+    { name: 'Mystery' },
+    { name: 'News' },
+    { name: 'Reality' },
+    { name: 'Sci-Fi & Fantasy' },
+    { name: 'Soap' },
+    { name: 'Talk' },
+    { name: 'War & Politics' },
+    { name: 'Western' },
 ]
 
 const movieGenres = [
@@ -288,11 +288,17 @@ const tvGenres = [
 ]
 
 const getGenreId = (genre) => {
-  return (
-    movieGenres.find(elem => elem.name === genre)
-    || tvGenres.find(elem => elem.name === genre)
-    || {}
-  ).id
+  if (movie) {
+    return (
+      movieGenres.find(elem => elem.name === genre)
+      || {}
+    ).id
+  } else {
+    return (
+      tvGenres.find(elem => elem.name === genre)
+      || {}
+    ).id
+  }
 }
 
 const amusedGifs = [
