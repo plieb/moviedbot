57b07c5a5b7c4d48abf85dd8e3b858e22d39adec
PE Lieb <pierre-edouard.lieb@sap.com>
2018-12-24 17:07:24 +0100

    [ADD] Port 5000

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // You can add your own tmdb token here (https://www.themoviedb.org/)
   MOVIEDB_TOKEN: process.env.MOVIEDB_TOKEN || '',
-  PORT: process.env.PORT || 5000,
+  PORT: 5000,
 };
