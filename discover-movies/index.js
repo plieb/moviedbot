5b585a976a51b471852e7c07ea73cc077064c70d
PE Lieb <pierre-edouard.lieb@sap.com>
2018-12-25 03:02:18 +0100

    [ADD] such as anything and fixes

--- discover-movies/index.js
+++ discover-movies/index.js
@@ -1,27 +1,104 @@
 const config = require('../config');
-const { discoverMovie } = require('./movieApi');
+const movieApi = require('./movieApi.js');
 const constants = require('./constants');
+const moment = require('moment');
+
 
 function loadMovieRoute(app) {
   app.post('/discover-movies', function(req, res) {
     console.log('[GET] /discover-movies');
-    const movie = req.body.conversation.memory['movie'];
-    const tv = req.body.conversation.memory['tv'];
+    const conversation = req.body.conversation;
+    const nlp = req.body.nlp;
+    console.log('======================================')
+    console.log(`Skill: ${conversation.skill}`)
+    console.log('======================================')
 
-    const kind = movie ? 'movie' : 'tv';
+    if (conversation.skill === 'anything') {
+      // Let's just start a search with no criteria
+      return movieApi.discoverMovie({})
+        .then(function(carouselle) {
+          res.json({
+            replies: carouselle,
+          });
+        })
+        .catch(function(err) {
+          console.error('movieApi::discoverMovie error: ', err);
+        });
+    } else if (conversation.skill === 'discover') {
+      //Getting memory values
+      const movie = conversation.memory['movie'];
+      const tv = conversation.memory['tv'];
+      const kind = tv ? 'tv' : 'movie';
 
-    const genre = req.body.conversation.memory['genre'];
-    const genreId = constants.getGenreId(genre.value);
+      const genre = conversation.memory['genre'];
+      const genreId = constants.getGenreId(genre.value);
 
-    const language = req.body.conversation.memory['language'];
-    const nationality = req.body.conversation.memory['nationality'];
+      const language = conversation.memory['language'];
+      const nationality = conversation.memory['nationality'];
+      const isoCode = language
+        ? language.short.toLowerCase()
+        : nationality.short.toLowerCase();
 
-    const isoCode = language
-      ? language.short.toLowerCase()
-      : nationality.short.toLowerCase();
+      const dateInterval = conversation.memory['interval']
+      const date = conversation.memory['datetime']
+      let year = null
+      let interval = null
+      if (date) {
+        year = moment(date.iso).year()
+      }
+      if (dateInterval) {
+        interval = {
+          begin: moment(dateInterval.begin).format('YYYY-MM-DD'),
+          end: moment(dateInterval.end).format('YYYY-MM-DD'),
+        }
+      }
 
-    return discoverMovie(kind, genreId, isoCode)
-      .then(function(carouselle) {
+      if (kind === 'movie') {
+        console.log('======================================')
+        console.log('Discover movie')
+        console.log('======================================')
+        return movieApi.discoverMovie({ genreId, isoCode, year, interval })
+          .then(function(carouselle) {
+            res.json({
+              replies: carouselle,
+            });
+          })
+          .catch(function(err) {
+            console.error('movieApi::discoverMovie error: ', err);
+          });
+      }
+      console.log('======================================')
+      console.log('Discover tv')
+      console.log('======================================')
+      return movieApi.discoverTv({ genreId, isoCode, year, interval })
+        .then(function(carouselle) {
+          res.json({
+            replies: carouselle,
+          });
+        })
+        .catch(function(err) {
+          console.error('movieApi::discoverMovie error: ', err);
+        });
+    } else if (conversation.skill === 'such-as' && nlp.entities['movie-name']) {
+      if (nlp.entities.tv) {
+        console.log('======================================')
+        console.log('Such as in tv')
+        console.log('======================================')
+        return movieApi.findShowSimilarTo(nlp.entities['movie-name'][0].value)
+          .then(function(carouselle) {
+            res.json({
+              replies: carouselle,
+            });
+          })
+          .catch(function(err) {
+            console.error('movieApi::discoverMovie error: ', err);
+          });
+      }
+      console.log('======================================')
+      console.log('Such as in movie')
+      console.log('======================================')
+      return movieApi.findMovieSimilarTo(nlp.entities['movie-name'][0].value)
+        .then(function(carouselle) {
         res.json({
           replies: carouselle,
         });
@@ -29,6 +106,11 @@ function loadMovieRoute(app) {
       .catch(function(err) {
         console.error('movieApi::discoverMovie error: ', err);
       });
+    } else {
+      return res.json({
+        replies: [{type: 'text', content: 'Can not help you with this right now'}],
+      });
+    }
   });
 }
 
--- discover-movies/movieApi.js
+++ discover-movies/movieApi.js
@@ -9,49 +9,125 @@ Array.prototype.shuffle = function () {
   return this
 }
 
-function discoverMovie(kind, genreId, language) {
-  return moviedbApiCall(kind, genreId, language).then(response =>
-    apiResultToCarousselle(response.data.results)
-  );
+const discoverMovie = ({ genreId, isoCode, year, interval }) => {
+  return moviedbApiCall('movie', {
+    with_genres: genreId,
+    primary_release_year: year,
+    with_original_language: isoCode,
+    'primary_release_date.gte': (interval || {}).start,
+    'primary_release_date.lte': (interval || {}).end,
+  })
+    .then((response) => apiResultToCarousselle(response, 'movie'))
 }
 
-function moviedbApiCall(kind, genreId, language) {
-  return axios.get(`https://api.themoviedb.org/3/discover/${kind}`, {
-    params: {
+const discoverTv = ({ genreId, isoCode, year, interval }) => {
+  return moviedbApiCall('tv', {
+    with_genres: genreId,
+    first_air_date_year: year,
+    with_original_language: isoCode,
+    'air_date.gte': (interval || {}).start,
+    'air_date.lte': (interval || {}).end,
+  })
+    .then((response) => apiResultToCarousselle(response, 'tv'))
+}
+
+const moviedbApiCall = (kind, params) => {
+  return Promise.all(
+    [1, 2, 3].map(page => axios.get(`https://api.themoviedb.org/3/discover/${kind}`, {
+      params: Object.assign({}, {
+        api_key: config.MOVIEDB_TOKEN,
+        sort_by: 'popularity.desc',
+        include_adult: false,
+        page,
+      }, params),
+    }))
+  ).then(res => res.reduce((sum, currentElem) => sum.concat(currentElem.data.results), []))
+}
+
+const findMovieSimilarTo = (movie) => {
+  return movieDbSearch('movie', { query: movie })
+    .then(elems => {
+      console.log(movie)
+      console.log(elems)
+      if (elems.length === 0) {
+        return [{
+          type: 'quickReplies',
+          content: {
+            title: 'Sorry, but I could not find any results for your request :(',
+            buttons: [{ title: 'Start over', value: 'Start over' }],
+          },
+        }]
+
+      }
+      return movieDbGetRecommendations(elems[0].id, 'movie')
+        .then((res) => apiResultToCarousselle(res, 'movie'))
+    })
+}
+
+const findShowSimilarTo = (movie) => {
+  return movieDbSearch('tv', { query: movie })
+    .then(elems => {
+      if (elems.length === 0) {
+        return [{
+          type: 'quickReplies',
+          content: {
+            title: 'Sorry, but I could not find any results for your request :(',
+            buttons: [{ title: 'Start over', value: 'Start over' }],
+          },
+        }]
+
+      }
+      return movieDbGetRecommendations(elems[0].id, 'tv')
+        .then((res) => apiResultToCarousselle(res, 'tv'))
+    })
+}
+
+const movieDbSearch = (kind, params = {}) => {
+  return axios.get(`https://api.themoviedb.org/3/search/${kind}`, {
+    params: Object.assign({}, {
       api_key: config.MOVIEDB_TOKEN,
-      sort_by: 'popularity.desc',
       include_adult: false,
-      with_genres: genreId,
-      with_original_language: language,
-    },
-  });
+      page: 1,
+    }, params),
+  })
+    .then(res => res.data.results)
 }
 
-function apiResultToCarousselle(results) {
-  if (results.length === 0) {
-    return [
-      {
-        type: 'quickReplies',
-        content: {
-          title: 'Sorry, but I could not find any results for your request :(',
-          buttons: [{ title: 'Start over', value: 'Start over' }],
-        },
-      },
-    ];
-  }
+const movieDbGetRecommendations = (id, kind, params = {}) => {
+  return Promise.all(
+    [1, 2, 3].map(page => axios.get(`https://api.themoviedb.org/3/${kind}/${id}/recommendations`, {
+      params: Object.assign({}, {
+        api_key: config.MOVIEDB_TOKEN,
+        page,
+      }, params),
+    }))
+  ).then(res => res.reduce((sum, currentElem) => sum.concat(currentElem.data.results), []))
 
-  const cards = results.slice(0, 10).map(e => ({
-    title: e.title || e.name,
-    subtitle: e.overview,
-    imageUrl: `https://image.tmdb.org/t/p/w600_and_h900_bestv2${e.poster_path}`,
-    buttons: [
-      {
+}
+
+const apiResultToCarousselle = (results, kind) => {
+  const cards = results.shuffle()
+    .slice(0, 10)
+    .map(e => ({
+      title: e.title || e.name,
+      subtitle: e.overview,
+      imageUrl: `https://image.tmdb.org/t/p/w600_and_h900_bestv2${e.poster_path}`,
+      buttons: [{
         type: 'web_url',
-        value: `https://www.themoviedb.org/movie/${e.id}`,
+        value: `https://www.themoviedb.org/${kind}/${e.id}`,
         title: 'View More',
+      }],
+    }))
+
+  if (cards.length === 0) {
+    return [{
+      type: 'quickReplies',
+      content: {
+        title: 'Sorry, but I could not find any results for your request :(',
+        buttons: [{ title: 'Start over', value: 'Start over' }],
       },
-    ],
-  }));
+    }]
+  }
 
   const lastMessage = [
     'Here\'s what I found for you!',
@@ -71,16 +147,12 @@ function apiResultToCarousselle(results) {
       content: lastMessage.shuffle()[0],
     },
     { type: 'carousel', content: cards },
-    {
-      type: 'quickReplies',
-      content: {
-        title: 'You can change your criterias if you want to!',
-        buttons: [{ title: 'Start over', value: 'Start over' }],
-      },
-    },
   ];
 }
 
 module.exports = {
   discoverMovie,
+  discoverTv,
+  findMovieSimilarTo,
+  findShowSimilarTo,
 };
